6a73cafc7871c581fd1fa33a5bc6ac5f57a66ca6
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2022-02-09 20:07:23 -0400

    Cep Finalizado 90/02/2022

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,19 @@
+
 function consultaCep(){
+    mostrarBarra();
     let cep = document.getElementById("cep").value
     let url = "https://viacep.com.br/ws/"+ cep + "/json/";
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
-            console.log(response);
-        //$("#logradouro").html(response.logradouro);
-       // $("#bairro").html(response.bairro);
-       // $("#localidade").html(response.localidade);
-      //  $("#uf").html(response.uf);
-        
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP"+": " + response.cep);
+            esconde();
+            mostarCep();    
         //document.getElementById("logradouro").innerHTML = response.logradouro;
        // document.getElementById("bairro").innerHTML = response.bairro;
        //document.getElementById("localidade").innerHTML = response.localidade;
@@ -19,3 +22,15 @@ function consultaCep(){
         }
     })
 }
+function esconde(){
+    $(".cep").hide();
+    $(".barra_progres").hide();
+}
+function mostarCep(){
+    $(".cep").show();
+   
+}
+ function mostrarBarra(){
+    $(".barra_progres").show();
+ }
+
